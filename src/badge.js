a067146072e6beccddf13a92e1fbc401d2f48c6f
FrankPSU27 <122505447+FrankPSU27@users.noreply.github.com>
2023-03-22 14:27:01 -0400

    Basic Badge Template and imported the simple-icon

--- src/badge.js
+++ src/badge.js
@@ -1,4 +1,5 @@
 import { LitElement, html, css } from 'lit';
+import '@lrnwebcomponents/simple-icon/simple-icon.js';
 class Badge extends LitElement {
     static properties = {
       header: { type: String },
@@ -6,24 +7,40 @@ class Badge extends LitElement {
   
     static styles = css`
 
-    .badges {
+    .blankbadge {
       width: 600px;
       border: 2px solid black;
       display: inline-flex;
       color: black;
       background-color: grey;
+          }
+    
+    .image {
+      width: 60px;
+      padding: 0px 15px 00px 0px;
+    }
+    
+    .details {
+       margin-top: 21px;
+    }
+    
+    h3 {
+       padding: 0px 10px 0px 0px;
     }
 `
     render() {
       return html`
-    <div class="badges">
-    <h3>The title </h3>
-    <summary>INFO</summary>
-    <div>
-      <ul>
-       <li> Stuff goes in here</li>
-      </ul>
-    </div>`;
+      <div class="blankbadge">
+      <img class="image" src=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRsOq0qlUMYbf6GrwFKPCYVIj1YIcMz8b5ag&usqp=CAU/>
+      <h3>Badge Name </h3>
+        <details class="details">
+          <summary></summary>
+            <div>
+              <ul>
+                <li> Stuff goes in here</li>
+              </ul>
+            </div>
+       </div>`;
    }
 }
   customElements.define('badge', Badge);
