8a6a037fc8d39a2c0ba0df7d2ad043bb6e7cbe06
pjstrauss12 <parkerstrauss21@gmail.com>
2023-03-23 12:19:00 -0400

    Beginning to set up the API

---
+++ api/badges.js
@@ -0,0 +1,12 @@
+export default async function handler(request, res) {
+
+    const badges = [
+        // this will be filled with the contents of the 5 badges once they are complete
+    ];
+    res.setHeader('Cache-Control', 'max-age=0, s-maxage=1800');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+    res.json(badges);
+  }
